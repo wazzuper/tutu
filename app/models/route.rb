a78689a2b1e4733aaa6e4cb368dee97f47029efc
artem <clifredd@gmail.com>
2017-01-24 23:36:21 +0500

    Validates in route model

--- app/models/route.rb
+++ app/models/route.rb
@@ -5,4 +5,19 @@ class Route < ApplicationRecord
   has_many :railway_stations, through: :railway_stations_routes
 
   validates :name, presence: true
+  validate :stations_count
+
+  before_validation :set_name
+
+  private
+
+  def set_name
+    self.name = "#{railway_stations.first.title} - #{railway_stations.last.title}"
+  end
+
+  def stations_count
+    if railway_stations.size < 2
+      errors.add(:base, "Маршрут должен содержать минимум 2 станции")
+    end
+  end
 end
