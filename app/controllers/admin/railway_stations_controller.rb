9c8b0b0e6ff1441536ee2111c4019b58ed534579
artem <clifredd@gmail.com>
2017-02-03 14:29:43 +0500

    Few fixes for railway stations controller and views

--- app/controllers/admin/railway_stations_controller.rb
+++ app/controllers/admin/railway_stations_controller.rb
@@ -2,52 +2,42 @@ class Admin::RailwayStationsController < Admin::BaseController
   before_action :set_railway_station, only: [:show, :edit, :update, :destroy, :update_position, :update_time]
   before_action :set_route, only: [:update_position, :update_time]
 
-  # GET /railway_stations
-  # GET /railway_stations.json
   def index
     @railway_stations = RailwayStation.all
   end
 
-  # GET /railway_stations/1
-  # GET /railway_stations/1.json
   def show
   end
 
-  # GET /railway_stations/new
   def new
     @railway_station = RailwayStation.new
   end
 
-  # GET /railway_stations/1/edit
   def edit
   end
 
-  # POST /railway_stations
-  # POST /railway_stations.json
   def create
     @railway_station = RailwayStation.new(railway_station_params)
 
     respond_to do |format|
       if @railway_station.save
-        format.html { redirect_to @railway_station, notice: 'Станция была успешно добавлена' }
-        format.json { render :show, status: :created, location: @railway_station }
+        format.html { redirect_to [:admin, @railway_station], notice: 'Станция была успешно добавлена' }
+        # format.json { render :show, status: :created, location: @railway_station }
       else
         format.html { render :new }
-        format.json { render json: @railway_station.errors, status: :unprocessable_entity }
+        # format.json { render json: @railway_station.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /railway_stations/1
-  # PATCH/PUT /railway_stations/1.json
   def update
     respond_to do |format|
       if @railway_station.update(railway_station_params)
-        format.html { redirect_to @railway_station, notice: 'Станция обновлена' }
-        format.json { render :show, status: :ok, location: @railway_station }
+        format.html { redirect_to [:admin, @railway_station], notice: 'Станция обновлена' }
+        # format.json { render :show, status: :ok, location: @railway_station }
       else
         format.html { render :edit }
-        format.json { render json: @railway_station.errors, status: :unprocessable_entity }
+        # format.json { render json: @railway_station.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -62,18 +52,15 @@ class Admin::RailwayStationsController < Admin::BaseController
     redirect_to @route
   end
 
-  # DELETE /railway_stations/1
-  # DELETE /railway_stations/1.json
   def destroy
     @railway_station.destroy
     respond_to do |format|
-      format.html { redirect_to railway_stations_url, notice: 'Станция удалена' }
-      format.json { head :no_content }
+      format.html { redirect_to admin_railway_stations_url, notice: 'Станция удалена' }
+      # format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_railway_station
       @railway_station = RailwayStation.find(params[:id])
     end
@@ -82,7 +69,6 @@ class Admin::RailwayStationsController < Admin::BaseController
       @route = Route.find(params[:route_id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def railway_station_params
       params.require(:railway_station).permit(:title)
     end
