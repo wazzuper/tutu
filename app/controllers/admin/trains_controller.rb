bd531c29bae1e1b9a205d2e40bbcbe678bf6386d
artem <clifredd@gmail.com>
2017-02-03 14:58:46 +0500

    Admin path for trains and wagons views, controllers

--- app/controllers/admin/trains_controller.rb
+++ app/controllers/admin/trains_controller.rb
@@ -1,68 +1,55 @@
 class Admin::TrainsController < Admin::BaseController
   before_action :set_train, only: [:show, :edit, :update, :destroy]
 
-  # GET /trains
-  # GET /trains.json
   def index
     @trains = Train.all
   end
 
-  # GET /trains/1
-  # GET /trains/1.json
   def show
   end
 
-  # GET /trains/new
   def new
     @train = Train.new
   end
 
-  # GET /trains/1/edit
   def edit
   end
 
-  # POST /trains
-  # POST /trains.json
   def create
     @train = Train.new(train_params)
 
     respond_to do |format|
       if @train.save
-        format.html { redirect_to @train, notice: 'Поезд был успешно создан' }
-        format.json { render :show, status: :created, location: @train }
+        format.html { redirect_to [:admin, @train], notice: 'Поезд был успешно создан' }
+        # format.json { render :show, status: :created, location: @train }
       else
         format.html { render :new }
-        format.json { render json: @train.errors, status: :unprocessable_entity }
+        # format.json { render json: @train.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /trains/1
-  # PATCH/PUT /trains/1.json
   def update
     respond_to do |format|
       if @train.update(train_params)
-        format.html { redirect_to @train, notice: 'Поезд был обновлен' }
-        format.json { render :show, status: :ok, location: @train }
+        format.html { redirect_to [:admin, @train], notice: 'Поезд был обновлен' }
+        # format.json { render :show, status: :ok, location: @train }
       else
         format.html { render :edit }
-        format.json { render json: @train.errors, status: :unprocessable_entity }
+        # format.json { render json: @train.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /trains/1
-  # DELETE /trains/1.json
   def destroy
     @train.destroy
     respond_to do |format|
-      format.html { redirect_to trains_url, notice: 'Поезд был удален' }
-      format.json { head :no_content }
+      format.html { redirect_to admin_trains_url, notice: 'Поезд был удален' }
+      # format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_train
       @train = Train.find(params[:id])
     end
--- app/controllers/admin/wagons_controller.rb
+++ app/controllers/admin/wagons_controller.rb
@@ -17,7 +17,7 @@ class Admin::WagonsController < Admin::BaseController
     @wagon = @train.wagons.new(wagon_params)
 
     if @wagon.save
-      redirect_to @train
+      redirect_to [:admin, @train]
     else
       render :new
     end
@@ -28,7 +28,7 @@ class Admin::WagonsController < Admin::BaseController
 
   def update
     if @wagon.update(wagon_params)
-      redirect_to @wagon.train
+      redirect_to [:admin, @wagon.train]
     else
       render :edit
     end
@@ -36,7 +36,7 @@ class Admin::WagonsController < Admin::BaseController
 
   def destroy
     @wagon.destroy
-    redirect_to wagons_path
+    redirect_to admin_wagons_path
   end
 
   private
