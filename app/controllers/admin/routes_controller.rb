921528f3a48791add23c14a4fdc8bf6bc53aa17a
artem <clifredd@gmail.com>
2017-02-03 14:39:47 +0500

    Admin path for route views and controller

--- app/controllers/admin/routes_controller.rb
+++ app/controllers/admin/routes_controller.rb
@@ -16,7 +16,7 @@ class Admin::RoutesController < Admin::BaseController
     @route = Route.new(route_params)
 
     if @route.save
-      redirect_to @route
+      redirect_to [:admin, @route]
     else
       render :new
     end
@@ -27,7 +27,7 @@ class Admin::RoutesController < Admin::BaseController
 
   def update
     if @route.update(route_params)
-      redirect_to @route
+      redirect_to [:admin, @route]
     else
       render :edit
     end
@@ -35,7 +35,7 @@ class Admin::RoutesController < Admin::BaseController
 
   def destroy
     @route.destroy
-    redirect_to routes_path
+    redirect_to admin_routes_path
   end
 
   private
