2cc52ad3baed79f74cfde55e5c446189eafbb810
artem <clifredd@gmail.com>
2017-02-09 23:57:38 +0500

    fixes for gmail

--- Gemfile
+++ Gemfile
@@ -35,7 +35,7 @@ gem 'devise'
 gem 'therubyracer'
 gem 'twitter-bootstrap-rails'
 gem 'rails_12factor'
-gem 'sendgrid'
+gem 'sendgrid-ruby'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: "fast-crag-72645.herokuapp.com" }
-  config.action_mailer.delivery_method = :sendgrid
-  config.action_mailer.sendgrid_settings = { api_token: ENV['SENDGRID_API_TOKEN'] }
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = false  
+  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.smtp_settings = {  
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "fast-crag-72645.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
